1b54b97afc0798dfd2dfa45796e4dc61fae073f9
aathirav06 <aathiravarma06@gmail.com>
2022-05-25 01:39:14 +0530

    Brands added

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import BrandsLove from '../components/BrandsLove/BrandsLove'
 import Header from "../components/common/Header/Header"
+import Experience from '../components/Experience/Experience'
 import FeelSpecial from '../components/FeelSpecial/FeelSpecial'
 import HeroSection from '../components/HeroSection/HeroSection'
 import ProductShowcase from '../components/ProductShowcase/ProductShowcase'
-
+import Security from '../components/Security/Security'
 
 const Homepage = () => {
   return (
@@ -13,6 +15,9 @@ const Homepage = () => {
 <HeroSection />
 <ProductShowcase/>
 <FeelSpecial/>
+<BrandsLove/>
+<Experience/>
+<Security/>
     </>
   )
 }
